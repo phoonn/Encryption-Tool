aaa944f404ceeab151a378e785e34bf7d2c2b7ec
vili96 <velina.ilieva.46@gmail.com>
2019-06-07 11:16:35 +0300

    Initial commit

---
+++ EncryptionTool/EncryptionTool/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace EncryptionTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(60, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Encrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(252, 220);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(102, 22);
+            this.tbKey.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(60, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Decrypt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 524);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ EncryptionTool/EncryptionTool/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptionTool
+{
+    public partial class Form1 : Form
+    {
+        static string filePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                label1.Text = openFileDialog1.FileName;
+                filePath = openFileDialog1.FileName;
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string key = tbKey.Text;
+            if (String.IsNullOrEmpty(key) || String.IsNullOrEmpty(filePath))
+            {
+                return;
+            }
+
+            // Encryption process
+            EncryptFile(filePath, filePath.Replace("test","test1"), key);
+
+
+
+
+        }
+
+        private void EncryptFile(string inputFile, string outputFile, string key)
+        {
+            try
+            {
+                byte[] keyBytes;
+                keyBytes = Encoding.Unicode.GetBytes(key);
+
+                Rfc2898DeriveBytes derivedKey = new Rfc2898DeriveBytes(key, keyBytes);
+
+                RijndaelManaged rijndaelCSP = new RijndaelManaged();
+                rijndaelCSP.BlockSize = 256;
+                rijndaelCSP.KeySize = 256;
+                rijndaelCSP.Padding = PaddingMode.ANSIX923; // fills out the encrypted block with zeros in the beginning of the file
+                
+
+                rijndaelCSP.Key = derivedKey.GetBytes(rijndaelCSP.KeySize / 8);
+                rijndaelCSP.IV = derivedKey.GetBytes(rijndaelCSP.BlockSize / 8);
+
+                ICryptoTransform encryptor = rijndaelCSP.CreateEncryptor();
+
+                FileStream inputFileStream = new FileStream(inputFile, FileMode.Open, FileAccess.Read);
+
+                byte[] inputFileData = new byte[(int)inputFileStream.Length];
+                inputFileStream.Read(inputFileData, 0, (int)inputFileStream.Length);
+
+                FileStream outputFileStream = new FileStream(outputFile, FileMode.Create, FileAccess.Write);
+
+                CryptoStream encryptStream = new CryptoStream(outputFileStream, encryptor, CryptoStreamMode.Write);
+                encryptStream.Write(inputFileData, 0, (int)inputFileStream.Length);
+                encryptStream.FlushFinalBlock();
+
+                rijndaelCSP.Clear();
+                encryptStream.Close();
+                inputFileStream.Close();
+                outputFileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Encryption Failed!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            MessageBox.Show("File Encryption Complete!");
+        }
+
+        private void DecryptFile(string inputFile, string outputFile, string key)
+        {
+            try
+            {
+                byte[] keyBytes = Encoding.Unicode.GetBytes(key);
+
+                Rfc2898DeriveBytes derivedKey = new Rfc2898DeriveBytes(key, keyBytes);
+
+                RijndaelManaged rijndaelCSP = new RijndaelManaged();
+                rijndaelCSP.BlockSize = 256;
+                rijndaelCSP.KeySize = 256;
+                rijndaelCSP.Padding = PaddingMode.Zeros; // fills out the encrypted block with zeros in the beginning of the file
+
+                rijndaelCSP.Key = derivedKey.GetBytes(rijndaelCSP.KeySize / 8);
+                rijndaelCSP.IV = derivedKey.GetBytes(rijndaelCSP.BlockSize / 8);
+                      
+                ICryptoTransform decryptor = rijndaelCSP.CreateDecryptor();
+
+                FileStream inputFileStream = new FileStream(inputFile, FileMode.Open, FileAccess.Read);
+
+                CryptoStream decryptStream = new CryptoStream(inputFileStream, decryptor, CryptoStreamMode.Read);
+
+                byte[] inputFileData = new byte[(int)inputFileStream.Length];
+                decryptStream.Read(inputFileData, 0, (int)inputFileStream.Length);
+
+                FileStream outputFileStream = new FileStream(outputFile, FileMode.Create, FileAccess.Write);
+                outputFileStream.Write(inputFileData, 0, inputFileData.Length);
+                outputFileStream.Flush();
+
+                rijndaelCSP.Clear();
+
+                decryptStream.Close();
+                inputFileStream.Close();
+                outputFileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Decryption Failed!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            MessageBox.Show("File Decryption Complete!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string key = tbKey.Text;
+            if (String.IsNullOrEmpty(key) || String.IsNullOrEmpty(filePath))
+            {
+                return;
+            }
+
+            // Encryption process
+            DecryptFile(filePath, filePath.Replace("test1","test2"), key);
+        }
+    }
+}
