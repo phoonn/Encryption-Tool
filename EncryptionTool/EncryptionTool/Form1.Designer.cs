3d0f4dd17a12f1de9b657345185770d8f17c9c83
vili96 <velina.ilieva.46@gmail.com>
2019-06-11 18:59:33 +0300

    validations, exception handling, sound effect added

--- EncryptionTool/EncryptionTool/Form1.Designer.cs
+++ EncryptionTool/EncryptionTool/Form1.Designer.cs
@@ -49,10 +49,10 @@
             // 
             // btn_browse
             // 
-            this.btn_browse.Location = new System.Drawing.Point(234, 33);
-            this.btn_browse.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_browse.Location = new System.Drawing.Point(312, 41);
+            this.btn_browse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_browse.Name = "btn_browse";
-            this.btn_browse.Size = new System.Drawing.Size(130, 29);
+            this.btn_browse.Size = new System.Drawing.Size(173, 36);
             this.btn_browse.TabIndex = 0;
             this.btn_browse.Text = "Browse File";
             this.btn_browse.UseVisualStyleBackColor = true;
@@ -60,10 +60,10 @@
             // 
             // btn_encrypt
             // 
-            this.btn_encrypt.Location = new System.Drawing.Point(178, 83);
-            this.btn_encrypt.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_encrypt.Location = new System.Drawing.Point(237, 102);
+            this.btn_encrypt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_encrypt.Name = "btn_encrypt";
-            this.btn_encrypt.Size = new System.Drawing.Size(130, 32);
+            this.btn_encrypt.Size = new System.Drawing.Size(173, 39);
             this.btn_encrypt.TabIndex = 2;
             this.btn_encrypt.Text = "Encrypt";
             this.btn_encrypt.UseVisualStyleBackColor = true;
@@ -71,18 +71,18 @@
             // 
             // tb_encryptionKey
             // 
-            this.tb_encryptionKey.Location = new System.Drawing.Point(29, 90);
-            this.tb_encryptionKey.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_encryptionKey.Location = new System.Drawing.Point(39, 111);
+            this.tb_encryptionKey.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tb_encryptionKey.Name = "tb_encryptionKey";
-            this.tb_encryptionKey.Size = new System.Drawing.Size(135, 20);
+            this.tb_encryptionKey.Size = new System.Drawing.Size(179, 22);
             this.tb_encryptionKey.TabIndex = 3;
             // 
             // btn_decrypt
             // 
-            this.btn_decrypt.Location = new System.Drawing.Point(178, 133);
-            this.btn_decrypt.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_decrypt.Location = new System.Drawing.Point(237, 164);
+            this.btn_decrypt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_decrypt.Name = "btn_decrypt";
-            this.btn_decrypt.Size = new System.Drawing.Size(130, 35);
+            this.btn_decrypt.Size = new System.Drawing.Size(173, 43);
             this.btn_decrypt.TabIndex = 4;
             this.btn_decrypt.Text = "Decrypt";
             this.btn_decrypt.UseVisualStyleBackColor = true;
@@ -90,45 +90,48 @@
             // 
             // tb_file
             // 
-            this.tb_file.Location = new System.Drawing.Point(29, 38);
-            this.tb_file.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_file.Location = new System.Drawing.Point(39, 47);
+            this.tb_file.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tb_file.Name = "tb_file";
-            this.tb_file.Size = new System.Drawing.Size(201, 20);
+            this.tb_file.Size = new System.Drawing.Size(267, 22);
             this.tb_file.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Location = new System.Drawing.Point(39, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.Size = new System.Drawing.Size(78, 17);
             this.label1.TabIndex = 6;
             this.label1.Text = "Choose file";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 72);
+            this.label2.Location = new System.Drawing.Point(39, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.Size = new System.Drawing.Size(103, 17);
             this.label2.TabIndex = 7;
             this.label2.Text = "Encryption Key";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 123);
+            this.label3.Location = new System.Drawing.Point(39, 151);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.Size = new System.Drawing.Size(100, 17);
             this.label3.TabIndex = 9;
             this.label3.Text = "Decription Key";
             // 
             // tb_decryptionKey
             // 
-            this.tb_decryptionKey.Location = new System.Drawing.Point(29, 141);
-            this.tb_decryptionKey.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_decryptionKey.Location = new System.Drawing.Point(39, 174);
+            this.tb_decryptionKey.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tb_decryptionKey.Name = "tb_decryptionKey";
-            this.tb_decryptionKey.Size = new System.Drawing.Size(135, 20);
+            this.tb_decryptionKey.Size = new System.Drawing.Size(179, 22);
             this.tb_decryptionKey.TabIndex = 8;
             // 
             // encryptionWorker
@@ -140,25 +143,28 @@
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(29, 185);
+            this.progressBar.Location = new System.Drawing.Point(39, 228);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(335, 23);
+            this.progressBar.Size = new System.Drawing.Size(447, 28);
             this.progressBar.TabIndex = 10;
             // 
             // timeWatchLbl
             // 
             this.timeWatchLbl.AutoSize = true;
-            this.timeWatchLbl.Location = new System.Drawing.Point(234, 220);
+            this.timeWatchLbl.Location = new System.Drawing.Point(312, 271);
+            this.timeWatchLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.timeWatchLbl.Name = "timeWatchLbl";
-            this.timeWatchLbl.Size = new System.Drawing.Size(74, 13);
+            this.timeWatchLbl.Size = new System.Drawing.Size(98, 17);
             this.timeWatchLbl.TabIndex = 11;
             this.timeWatchLbl.Text = "Time Elapsed:";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(407, 252);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(543, 334);
             this.Controls.Add(this.timeWatchLbl);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.label3);
@@ -171,7 +177,7 @@
             this.Controls.Add(this.btn_encrypt);
             this.Controls.Add(this.btn_browse);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.Text = "Encryption Tool";
             this.ResumeLayout(false);
--- EncryptionTool/EncryptionTool/Form1.cs
+++ EncryptionTool/EncryptionTool/Form1.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Media;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,19 +29,25 @@ namespace EncryptionTool
         private void btn_browse_Click(object sender, EventArgs e)
         {
             DialogResult result = openFileDialog1.ShowDialog();
-            if (result == DialogResult.OK) // Test result.
+            if (result == DialogResult.OK)
             {
                 tb_file.Text = openFileDialog1.FileName;
             }
-            Console.WriteLine(result);
         }
 
         private void btn_encrypt_Click(object sender, EventArgs e)
         {
-            key = tb_encryptionKey.Text;
+            key = tb_encryptionKey.Text.Trim();
             filePath = tb_file.Text;
-            if (String.IsNullOrEmpty(key) || String.IsNullOrEmpty(filePath))
+            if (String.IsNullOrEmpty(key))
             {
+                MessageBox.Show("Key is empty!");
+                return;
+
+            }
+            else if (String.IsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("Invalid path!");
                 return;
             }
 
@@ -49,27 +56,32 @@ namespace EncryptionTool
                 MessageBox.Show("File not found!");
                 return;
             }
-
             stop.Start();
 
             btn_browse.Enabled = false;
             btn_decrypt.Enabled = false;
             btn_encrypt.Enabled = false;
-            tb_encryptionKey.Enabled = false;
-            tb_decryptionKey.Enabled = false;
-            tb_file.Enabled = false;
+            tb_encryptionKey.ReadOnly = true;
+            tb_decryptionKey.ReadOnly = true;
+            tb_file.ReadOnly = true;
             // Encryption process
             encryptionWorker.RunWorkerAsync(argument: "encrypt");
-
 
         }
 
         private void btn_decrypt_Click(object sender, EventArgs e)
         {
-            key = tb_decryptionKey.Text;
+            key = tb_decryptionKey.Text.Trim();
             filePath = tb_file.Text;
-            if (String.IsNullOrEmpty(key) || String.IsNullOrEmpty(filePath))
+            if (String.IsNullOrEmpty(key))
             {
+                MessageBox.Show("Key is empty!");
+                return;
+
+            }
+            else if (String.IsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("Invalid path!");
                 return;
             }
 
@@ -93,69 +105,73 @@ namespace EncryptionTool
 
         private void EncryptFileStream(string filePath, string key)
         {
-            //try
-            //{
-            byte[] salt = GenerateSalt();
+            string tempFile = filePath.Substring(0, filePath.LastIndexOf(".")) + "encrypted" + filePath.Substring(filePath.LastIndexOf("."));
 
-            Rfc2898DeriveBytes derivedKey = new Rfc2898DeriveBytes(key, salt, 5000);
-
-            RijndaelManaged rijndaelCSP = new RijndaelManaged();
-            rijndaelCSP.BlockSize = 128;
-            rijndaelCSP.KeySize = 256;
-            rijndaelCSP.Padding = PaddingMode.ANSIX923; // fills out the encrypted block with zeros in the beginning of the file
-
-
-            rijndaelCSP.Key = derivedKey.GetBytes(rijndaelCSP.KeySize / 8);
-            rijndaelCSP.IV = derivedKey.GetBytes(rijndaelCSP.BlockSize / 8);
-
-            ICryptoTransform encryptor = rijndaelCSP.CreateEncryptor();
-
-            FileStream inputFileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-
-            long fileSizeBytes = inputFileStream.Length;
-
-            string tempFile = filePath.Substring(0, filePath.LastIndexOf(".")) + "encrypted1" + filePath.Substring(filePath.LastIndexOf("."));
-
-            FileStream outputFileStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write);
-            
-            outputFileStream.Write(salt, 0, salt.Length);
-
-            CryptoStream encryptStream = new CryptoStream(outputFileStream, encryptor, CryptoStreamMode.Write);
-
-
-            byte[] buffer = new byte[mbSize];
-            long i = 0;
-            int num;
-            while ((num = inputFileStream.Read(buffer, 0, mbSize)) > 0)
+            try
             {
-                encryptStream.Write(buffer, 0, num);
+                byte[] salt = GenerateSalt();
+
+                Rfc2898DeriveBytes derivedKey = new Rfc2898DeriveBytes(key, salt, 5000);
+
+                RijndaelManaged rijndaelCSP = new RijndaelManaged();
+                rijndaelCSP.BlockSize = 128;
+                rijndaelCSP.KeySize = 256;
+                rijndaelCSP.Padding = PaddingMode.ANSIX923;
+
+
+                rijndaelCSP.Key = derivedKey.GetBytes(rijndaelCSP.KeySize / 8);
+                rijndaelCSP.IV = derivedKey.GetBytes(rijndaelCSP.BlockSize / 8);
+
+                ICryptoTransform encryptor = rijndaelCSP.CreateEncryptor();
+
+                FileStream inputFileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+                long fileSizeBytes = inputFileStream.Length;
+
+
+                FileStream outputFileStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write);
+
+                outputFileStream.Write(salt, 0, salt.Length);
+
+                CryptoStream encryptStream = new CryptoStream(outputFileStream, encryptor, CryptoStreamMode.Write);
+
+
+                byte[] buffer = new byte[mbSize];
+                long i = 0;
+                int num;
+                while ((num = inputFileStream.Read(buffer, 0, mbSize)) > 0)
+                {
+                    encryptStream.Write(buffer, 0, num);
+                    outputFileStream.Flush();
+
+                    i++;
+                    long progress = 100 * (mbSize * i);
+                    progress = progress / fileSizeBytes;
+                    if (progress > 100)
+                        progress = 100;
+                    encryptionWorker.ReportProgress((int)progress);
+                }
+                inputFileStream.Close();
+                
+                encryptStream.FlushFinalBlock();
                 outputFileStream.Flush();
 
-                i++;
-                long progress = 100 * (mbSize * i);
-                progress = progress / fileSizeBytes;
-                if (progress > 100)
-                    progress = 100;
-                encryptionWorker.ReportProgress((int)progress);
+                encryptStream.Close();
+                outputFileStream.Close();
+                inputFileStream.Close();
+
+                File.Replace(tempFile, filePath, null);
             }
-            inputFileStream.Close();
+            catch (Exception ex)
+            {
+                File.Delete(tempFile);
+                MessageBox.Show(ex.Message, "Encryption Failed!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
-
-            encryptStream.FlushFinalBlock();
-            outputFileStream.Flush();
-            encryptStream.Close();
-            outputFileStream.Close();
-            inputFileStream.Close();
-
-            File.Replace(tempFile, filePath, null);
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message, "Encryption Failed!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    return;
-            //}
 
             stop.Stop();
+            playSimpleSound();
             MessageBox.Show("File Encryption Complete!");
         }
 
@@ -172,74 +188,78 @@ namespace EncryptionTool
 
         private void DecryptFileStream(string filePath, string key)
         {
-            //try
-            //{
-            //byte[] keyBytes;
-            //keyBytes = Encoding.Unicode.GetBytes(key);
-
-            FileStream inputFileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-
-            byte[] salt = new byte[32];
-            inputFileStream.Read(salt, 0, salt.Length);
-
-            Rfc2898DeriveBytes derivedKey = new Rfc2898DeriveBytes(key, salt, 5000);
-
-            RijndaelManaged rijndaelCSP = new RijndaelManaged();
-            rijndaelCSP.BlockSize = 128;
-            rijndaelCSP.KeySize = 256;
-            rijndaelCSP.Padding = PaddingMode.ANSIX923; // fills out the encrypted block with zeros in the beginning of the file
-
-
-            rijndaelCSP.Key = derivedKey.GetBytes(rijndaelCSP.KeySize / 8);
-            rijndaelCSP.IV = derivedKey.GetBytes(rijndaelCSP.BlockSize / 8);
-
-            ICryptoTransform encryptor = rijndaelCSP.CreateDecryptor();
-
-
-            long fileSizeBytes = inputFileStream.Length;
-
-            string tempFile = filePath.Substring(0, filePath.LastIndexOf(".")) + "decrypted1" + filePath.Substring(filePath.LastIndexOf("."));
-
-            FileStream outputFileStream = new FileStream(tempFile, FileMode.Append, FileAccess.Write);
-
-            CryptoStream encryptStream = new CryptoStream(outputFileStream, encryptor, CryptoStreamMode.Write);
-
-            byte[] buffer = new byte[mbSize];
-            long i = 0;
-            int num;
-            while ((num = inputFileStream.Read(buffer, 0, mbSize)) > 0)
+            string tempFile = filePath.Substring(0, filePath.LastIndexOf(".")) + "decrypted" + filePath.Substring(filePath.LastIndexOf("."));
+            try
             {
-                encryptStream.Write(buffer, 0, num);
-                outputFileStream.Flush();
-                
-                i++;
-                long progress = 100 * (mbSize * i);
-                progress = progress / fileSizeBytes;
-                if (progress > 100)
-                    progress = 100;
-                encryptionWorker.ReportProgress((int)progress);
+                using (FileStream inputFileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] salt = new byte[32];
+                    inputFileStream.Read(salt, 0, salt.Length);
+
+                    Rfc2898DeriveBytes derivedKey = new Rfc2898DeriveBytes(key, salt, 5000);
+
+                    ICryptoTransform encryptor = null;
+                    using (RijndaelManaged rijndaelCSP = new RijndaelManaged())
+                    {
+                        rijndaelCSP.BlockSize = 128;
+                        rijndaelCSP.KeySize = 256;
+                        rijndaelCSP.Padding = PaddingMode.ANSIX923; // fills out the encrypted block with zeros in the beginning of the file
+
+                        rijndaelCSP.Key = derivedKey.GetBytes(rijndaelCSP.KeySize / 8);
+                        rijndaelCSP.IV = derivedKey.GetBytes(rijndaelCSP.BlockSize / 8);
+
+                        encryptor = rijndaelCSP.CreateDecryptor();
+                    }
+
+                    long fileSizeBytes = inputFileStream.Length;
+
+
+                    using (FileStream outputFileStream = new FileStream(tempFile, FileMode.Append, FileAccess.Write))
+                    {
+                        using (var encryptStream = new CryptoStream(outputFileStream, encryptor, CryptoStreamMode.Write))
+                        {
+                            byte[] buffer = new byte[mbSize];
+                            long i = 0;
+                            int num;
+                            while ((num = inputFileStream.Read(buffer, 0, mbSize)) > 0)
+                            {
+                                encryptStream.Write(buffer, 0, num);
+                                outputFileStream.Flush();
+
+                                i++;
+                                long progress = 100 * (mbSize * i);
+                                progress = progress / fileSizeBytes;
+                                if (progress > 100)
+                                    progress = 100;
+                                encryptionWorker.ReportProgress((int)progress);
+                            }
+
+                            encryptStream.FlushFinalBlock();
+                            outputFileStream.Flush();
+                        }
+                    }
+                }
+
+                File.Replace(tempFile, filePath, null);
             }
-            inputFileStream.Close();
-
-
-            encryptStream.FlushFinalBlock();
-            outputFileStream.Flush();
-            encryptStream.Close();
-            outputFileStream.Close();
-            inputFileStream.Close();
-
-            File.Replace(tempFile, filePath, null);
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message, "Encryption Failed!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    return;
-            //}
-
+            catch (System.Security.Cryptography.CryptographicException ex)
+            {
+                stop.Stop();
+                MessageBox.Show("Wrong decryption key!", "Key Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                File.Delete(tempFile);
+                return;
+            }
+            catch (Exception ex)
+            {
+                stop.Stop();
+                MessageBox.Show(ex.Message, "Encryption Failed!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                File.Delete(tempFile);
+                return;
+            }
             stop.Stop();
             MessageBox.Show("File Decryption Complete!");
         }
-        
+
 
         private void encryptionWorker_DoWork(object sender, DoWorkEventArgs e)
         {
@@ -259,15 +279,20 @@ namespace EncryptionTool
             key = String.Empty;
             progressBar.Value = 100;
             string elapsedTime = stop.Elapsed.ToString();
-            timeWatchLbl.Text = "Elapsed Time: " + elapsedTime.Substring(0,elapsedTime.LastIndexOf(".")+3);
+            timeWatchLbl.Text = "Elapsed Time: " + elapsedTime.Substring(0, elapsedTime.LastIndexOf(".") + 3);
             stop.Reset();
-            
+
             btn_browse.Enabled = true;
             btn_decrypt.Enabled = true;
             btn_encrypt.Enabled = true;
             tb_encryptionKey.ReadOnly = false;
             tb_decryptionKey.ReadOnly = false;
             tb_file.ReadOnly = false;
+        }
+        private void playSimpleSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Directory.GetCurrentDirectory()+"\\wow_work_complete.wav");
+            simpleSound.Play();
         }
     }
 }
